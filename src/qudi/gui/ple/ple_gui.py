9ff28dc9c2d49b07a171a6bc85fa701549d8292e
Vladislav <bushmakinvls@gmail.com>
2022-07-14 11:36:10 +0200

    microwave_udpate ple gui

--- src/qudi/gui/ple/ple_gui.py
+++ src/qudi/gui/ple/ple_gui.py
@@ -183,7 +183,8 @@ class PLEScanGui(GuiBase):
         self._microwave_logic.sigCwStateUpdated.connect(
             self._mw.microwave_widget.enable_microwave
         )
-
+        self._microwave_logic.sigCwParametersUpdated.emit(self._microwave_logic.cw_parameters)
+        
     def setup_fit_widget(self):
         self._fit_dockwidget = PleFitDockWidget(parent=self._mw, fit_container=self._scanning_logic._fit_container)
         self._fit_config_dialog = FitConfigurationDialog(parent=self._mw,
