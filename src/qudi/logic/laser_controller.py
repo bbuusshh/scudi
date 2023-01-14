210768ea1943b5e9baff026d448ff9af741dd9e4
Vladislav <bushmakinvls@gmail.com>
2023-01-14 22:37:54 +0100

    laser controller

--- src/qudi/logic/laser_controller.py
+++ src/qudi/logic/laser_controller.py
@@ -14,15 +14,15 @@ from qudi.hardware.cwave.cwave_api import PiezoChannel, StatusBit, PiezoMode, Ex
 from PySide2 import QtCore
 
 class LaserControllerLogic(LogicBase):
-    motor_pulser = Connector(interface='DigitalSwitchNI')
-    ao_laser_control = Connector(interface='NIXSeriesAnalogOutput')
+    motor_pulser = Connector(name = 'motor_pulser', interface='DigitalSwitchNI')
+    ao_laser_control = Connector(name = 'ao_laser_control', interface='NIXSeriesAnalogOutput')
 
-    def __init__(self):
-        self._motor_pulser = self.motor_pulser()
-        self._ao_laser_control = self.ao_laser_control()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def on_activate(self) -> None:
-        return 
+        self._motor_pulser = self.motor_pulser()
+        self._ao_laser_control = self.ao_laser_control() 
 
     def on_deactivate(self) -> None:
         self._ao_laser_control.set_activity_state(active=False)
@@ -30,15 +30,15 @@ class LaserControllerLogic(LogicBase):
 
     def set_thin_etalon_voltage(self,v):
         self._ao_laser_control.set_activity_state(active=True)
-        self._ao_laser_control.set_setpoint(channel = 'a0', value = v)
+        self._ao_laser_control.set_setpoint(channel = 'ao0', value = v)
         self._ao_laser_control.set_activity_state(active=False)
 
     def set_motor_direction(self,sign):
         self._ao_laser_control.set_activity_state(active=True)
         if sign > 0:
-            self._ao_laser_control.set_setpoint(channel = 'a2', value = 5)
+            self._ao_laser_control.set_setpoint(channel = 'ao2', value = 5)
         elif sign < 0 :
-            self._ao_laser_control.set_voltage(channel = 'a2', value = -5)
+            self._ao_laser_control.set_setpoint(channel = 'ao2', value = -5)
         self._ao_laser_control.set_activity_state(active=False)
     def move_motor_pulse(self):
         self._motor_pulser.set_state(switch="motor", state='Low') #'High' should be pulsing?
