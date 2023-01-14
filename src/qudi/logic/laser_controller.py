2cb76904845b5f42b9a149337e2741778a55f988
bbuusshh <bushmakinvls@gmail.com>
2023-01-14 22:07:37 +0100

    laser controller

--- src/qudi/logic/laser_controller.py
+++ src/qudi/logic/laser_controller.py
@@ -16,17 +16,26 @@ from PySide2 import QtCore
 class LaserControllerLogic(LogicBase):
     motor_pulser = Connector(interface='DigitalSwitchNI')
     ao_laser_control = Connector(interface='NIXSeriesAnalogOutput')
-    power_controller = Connector(interface='')
+
     def __init__(self):
         self._motor_pulser = self.motor_pulser()
         self._ao_laser_control = self.ao_laser_control()
 
+    def on_activate(self) -> None:
+        return 
+
+    def on_deactivate(self) -> None:
+        return 
+
     def set_thin_etalon_voltage(self,v):
-        self._ao_laser_control.set_voltage(a0 = v)
+        self._ao_laser_control.set_voltage(channel = 'a0', value = v)
+        
     def set_motor_direction(self,sign):
         if sign > 0:
-            self._ao_laser_control(a2 = 5)
+            self._ao_laser_control.set_voltage(channel = 'a2', value = 5)
         elif sign < 0 :
-            self._ao_laser_control(a2=-5)
+            self._ao_laser_control.set_voltage(channel = 'a2', value = -5)
+
     def move_motor_pulse(self):
-        self._motor_pulser(self.ni._stepper_pulse_channel)
+        self._motor_pulser.set_state(switch="motor", state='Low') #'High' should be pulsing?
+        
