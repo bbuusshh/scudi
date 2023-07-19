2bb81925c91426590f54b7da60e94c08305d623a
Vlad Bushmakin <bushmakinvls@gmail.com>
2023-07-19 10:30:18 +0200

    check optimized on rsquared

--- src/qudi/logic/ple/optimize_logic.py
+++ src/qudi/logic/ple/optimize_logic.py
@@ -379,7 +379,7 @@ class PLEOptimizeScannerLogic(LogicBase):
                 try:
                     if data.scan_dimension == 1:
                         x = np.linspace(*data.scan_range[0], data.scan_resolution[0])
-                        opt_pos, fit_data, fit_res = self._get_pos_from_1d_gauss_fit(
+                        opt_pos, fit_data, fit_res = self._get_pos_from_1d_lorentian_fit(
                             x,
                             data.data[self._data_channel]
                         )
@@ -396,7 +396,7 @@ class PLEOptimizeScannerLogic(LogicBase):
                                         # Abort optimize if fit failed
                     self._last_fit_results = fit_res
                     if ((fit_data is None) 
-                        or (fit_res is None)):#or (fit_res is not None and fit_res.rsquared < self._min_r_squared)):
+                        or (fit_res is None) or (fit_res is not None and fit_res.rsquared < self._min_r_squared)):
                         self.log.warning("Stopping optimization due to failed fit.")
                         self.stop_optimize()
                         return
@@ -499,6 +499,7 @@ class PLEOptimizeScannerLogic(LogicBase):
         self._last_fit_results = fit_result
         return (fit_result.best_values['center'],), fit_result.best_fit, fit_result
 
+    
 
 class OptimizerScanSequence():
     def __init__(self, axes, sequence=None):
